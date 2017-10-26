720032c52bf8bfb87570055bd69d237da588abef
MicaelaMelek <micaelamelek@gmail.com>
2017-10-26 00:23:01 -0300

    singup model, controller and migration

---
+++ app/controllers/singups_controller.rb
@@ -0,0 +1,2 @@
+class SingupsController < ApplicationController
+end
---
+++ db/migrate/20171026024035_create_singups.rb
@@ -0,0 +1,8 @@
+class CreateSingups < ActiveRecord::Migration[5.1]
+  def change
+    create_table :singups do |t|
+      t.string :email
+      t.timestamps
+    end
+  end
+end
